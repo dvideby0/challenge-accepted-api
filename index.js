37ff07d37584f72076a81c716e728e28dc71bf43
Eric Nograles <ENograles@352inc.com>
2016-09-09 16:26:03 -0400

    adds multer

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const YouTubeInitializer = require('./youtube');
+const multer  = require('multer');
+var fs = require('fs');
+var path = require('path');
+var upload = multer({
+  dest: './uploads'
+});
 
 const response = [
   {id: 1, name: 'challenge 1', youtube_id: 'jdYJf_ybyVo'},
@@ -7,9 +14,50 @@ const response = [
   {id: 3, name: 'challenge 3', youtube_id: '7I6eI7hUruY'}
 ];
 
+
 app.get('/*', function(req, res) {
   console.log('I was called');
   res.status(200).json(response);
 });
 
-app.listen(9088);
+app.post('/media', upload.single('video'), function(req, res) {
+  // TODO: Buffer req.file.path
+  var video = fs.createReadStream(req.file.path);
+  var youTubeUpload = YouTube.videos.insert({
+    resource: {
+      // Video title and description
+      snippet: {
+        title: "Testing YoutTube API NodeJS module",
+        description: "Test video upload via YouTube API"
+      },
+      status: {
+        privacyStatus: "private"
+      }
+    }
+    // This is for the callback function
+    ,
+    part: "snippet,status"
+
+    // Create the readable stream to upload the video
+    ,
+    media: {
+      body: video
+    }
+  }, (err, data) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.status(200).json(response);
+    }
+  });
+
+  // setInterval(function () {
+  //   Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
+  // }, 250);
+});
+
+YouTubeInitializer.bootstrap()
+  .then(YouTube => {
+    global.YouTube = YouTube;
+    app.listen(9088);
+  });
--- youtube.js
+++ youtube.js
@@ -13,72 +13,79 @@
  * */
 
 const Youtube = require("youtube-api"),
-    fs = require("fs"),
-    readJson = require("r-json"),
-    Lien = require("lien"),
-    Logger = require("bug-killer"),
-    opn = require("opn"),
-    prettyBytes = require("pretty-bytes");
+  fs = require("fs"),
+  readJson = require("r-json"),
+  Lien = require("lien"),
+  Logger = require("bug-killer"),
+  opn = require("opn"),
+  prettyBytes = require("pretty-bytes"),
+  Promise = require('bluebird');
 
-// I downloaded the file from OAuth2 -> Download JSON
-const CREDENTIALS = readJson(`${__dirname}/credentials.json`);
+module.exports = {
+  bootstrap: bootstrap
+};
 
-// Init lien server
-let server = new Lien({
-    host: "localhost",
-    port: 5000
-});
+function bootstrap() {
+  return new Promise(
+    (resolve, reject) => {
+      const CREDENTIALS = readJson(`${__dirname}/credentials.json`);
+      let server = new Lien({
+        host: "localhost",
+        port: 5000
+      });
 
-// Authenticate
-// You can access the Youtube resources via OAuth2 only.
-// https://developers.google.com/youtube/v3/guides/moving_to_oauth#service_accounts
-let oauth = Youtube.authenticate({
-    type: "oauth",
-    client_id: CREDENTIALS.web.client_id,
-    client_secret: CREDENTIALS.web.client_secret,
-    redirect_url: CREDENTIALS.web.redirect_uris[0]
-});
+      let oauth = Youtube.authenticate({
+        type: "oauth",
+        client_id: CREDENTIALS.web.client_id,
+        client_secret: CREDENTIALS.web.client_secret,
+        redirect_url: CREDENTIALS.web.redirect_uris[0]
+      });
 
+      Logger.log("Got the tokens.");
+      var creds = {
+        access_token: "ya29.Ci9ZA4SF0rUA-LOtJH4TDgPImULokDq1cTCDF_-7CbiC4s_5GQpudOZ4PZ8-6UpSVA",
+        token_type: "Bearer",
+        expiry_date: 1473447768602
+      };
 
-
-Logger.log("Got the tokens.");
-var creds = {
-    access_token: "ya29.Ci9ZA4SF0rUA-LOtJH4TDgPImULokDq1cTCDF_-7CbiC4s_5GQpudOZ4PZ8-6UpSVA",
-    token_type: "Bearer",
-    expiry_date: 1473447768602
+      oauth.setCredentials(creds);
+      console.log('YouTube initialized');
+      resolve(Youtube);
+    }
+  );
 }
-oauth.setCredentials(creds);
 
 
-// lien.end("The video is being uploaded. Check out the logs in the terminal.");
+// // lien.end("The video is being uploaded. Check out the logs in the terminal.");
+//
+// var req = Youtube.videos.insert({
+//     resource: {
+//         // Video title and description
+//         snippet: {
+//             title: "Testing YoutTube API NodeJS module",
+//             description: "Test video upload via YouTube API"
+//         }
+//         // I don't want to spam my subscribers
+//         ,
+//         status: {
+//             privacyStatus: "private"
+//         }
+//     }
+//     // This is for the callback function
+//     ,
+//     part: "snippet,status"
+//
+//     // Create the readable stream to upload the video
+//     ,
+//     media: {
+//         body: fs.createReadStream("video.m4v")
+//     }
+// }, (err, data) => {
+//     console.log("Done.");
+//     process.exit();
+// });
+//
+// setInterval(function() {
+//     Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
+// }, 250);
 
-var req = Youtube.videos.insert({
-    resource: {
-        // Video title and description
-        snippet: {
-            title: "Testing YoutTube API NodeJS module",
-            description: "Test video upload via YouTube API"
-        }
-        // I don't want to spam my subscribers
-        ,
-        status: {
-            privacyStatus: "private"
-        }
-    }
-    // This is for the callback function
-    ,
-    part: "snippet,status"
-
-    // Create the readable stream to upload the video
-    ,
-    media: {
-        body: fs.createReadStream("video.m4v")
-    }
-}, (err, data) => {
-    console.log("Done.");
-    process.exit();
-});
-
-setInterval(function() {
-    Logger.log(`${prettyBytes(req.req.connection._bytesDispatched)} bytes uploaded.`);
-}, 250);

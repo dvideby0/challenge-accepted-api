0c9be0476e17ef7600cb703acd943231bc296db7
dvideby0 <rbrookfield@352inc.com>
2016-09-09 11:56:34 -0400

    adding post endpoint

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const response = [
   {id: 1, name: 'challenge 1', youtube_id: 'jdYJf_ybyVo'},
   {id: 2, name: 'Doing some crazy shit', youtube_id: 'a8dUPENLs70'},
   {id: 3, name: 'challenge 3', youtube_id: '7I6eI7hUruY'}
 ];
 
+app.use(bodyParser.json({limit: '1mb'}));
+
 app.get('/*', function(req, res) {
   console.log('I was called');
   res.status(200).json(response);
+});
+
+app.post('/*', function(req, res) {
+  console.log(req.body);
+  res.status(200).json({success: true});
 });
 
 const port = process.env.PORT || 9088;

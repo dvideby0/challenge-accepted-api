0d785b96a449d3fe0596ccdca6194e533abeb7e3
dvideby0 <rbrookfield@352inc.com>
2016-09-09 09:38:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const response = [
+  {id: 1, name: 'challenge 1', youtube_id: 'jdYJf_ybyVo'},
+  {id: 2, name: 'Doing some crazy shit', youtube_id: 'a8dUPENLs70'},
+  {id: 3, name: 'challenge 3', youtube_id: '7I6eI7hUruY'}
+];
+
+app.get('/*', function(req, res) {
+  console.log('I was called');
+  res.status(200).json(response);
+});
+
+app.listen(9088);
